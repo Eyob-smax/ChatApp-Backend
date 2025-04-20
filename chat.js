f1f788aa28ebfcc532a43c8e743cb9da3aadffb5
Eyoba_Smax <eyobsmax@gmail.com>
2025-04-21 02:17:16 +0300

    Initial commit

---
+++ chat.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  },
+});
+
+io.on("connection", (socket) => {
+  // console.log("User connected " + socket.id);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("message", data);
+  });
+});
+
+httpServer.listen(5000, () =>
+  console.log("Server running at http://localhost:5000")
+);
