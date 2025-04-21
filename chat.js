7cd4222d098561108aa2f8cd6e6a5232024f6d3f
Eyoba_Smax <eyobsmax@gmail.com>
2025-04-21 11:56:59 +0300

    Adding typing indicator

--- chat.js
+++ chat.js
@@ -11,11 +11,39 @@ const io = new Server(httpServer, {
   },
 });
 
+const typingUsers = new Set();
+
 io.on("connection", (socket) => {
-  // console.log("User connected " + socket.id);
+  // console.log(`User connected: ${socket.id}`);
 
   socket.on("message", (data) => {
     socket.broadcast.emit("message", data);
+
+    if (typingUsers.has(socket.id)) {
+      typingUsers.delete(socket.id);
+      socket.broadcast.emit("stopTyping", { userId: socket.id });
+    }
+  });
+
+  socket.on("typing", () => {
+    if (!typingUsers.has(socket.id)) {
+      typingUsers.add(socket.id);
+      socket.broadcast.emit("typing", { userId: socket.id });
+    }
+  });
+
+  socket.on("stopTyping", () => {
+    if (typingUsers.has(socket.id)) {
+      typingUsers.delete(socket.id);
+      socket.broadcast.emit("stopTyping", { userId: socket.id });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (typingUsers.has(socket.id)) {
+      typingUsers.delete(socket.id);
+      socket.broadcast.emit("stopTyping", { userId: socket.id });
+    }
   });
 });
 
